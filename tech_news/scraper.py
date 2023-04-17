daa6f6211d0fd9d5217e8c9560f54f3c4bf681af
Leandro Paiva <leandroapaiva@yahoo.com>
2023-04-17 16:04:47 -0300

    ✔ Feat: Cria a função scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,10 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    next_page = Selector(html_content)
+    link = next_page.css("a.next::attr(href)").get()
+    return link
+    # https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/290e715d-73e3-4b2d-a3c7-4fe113474070/section/7e82ac53-a588-412b-95a5-19727d70ed3a/day/9488d307-4a72-4c82-887f-d860ad20a1af/lesson/e9c42a21-2d88-4ef0-b3c9-f60e8f81235a
 
 
 # Requisito 4
